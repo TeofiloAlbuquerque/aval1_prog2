777c11cdd28d3a64f0319c5bb23060febd950cda
TeofiloAlbuquerque <contatoteofilosantos@gmail.com>
2023-06-01 16:17:19 -0300

    Adcionada a pasta data, criada a branch testes para trabalho colaborativo e iniciado a classe XML_data

--- lib/xml_data.dart
+++ lib/xml_data.dart
@@ -1,3 +1,14 @@
+import 'dart:io';
 import 'package:aval1_prog2/data.dart';
+import 'package:xml/xml.dart';
 
-class XmlData extends Data {}
+class XmlData extends Data {
+  final xmlFilePath = 'lib/data/sample.xml';
+  @override
+  void load(){}
+  final xmlFile = File(xmlFilePath);
+  final xmlString = xmlFile.readAsLinesSync();
+
+  final document = XmlDocument.parse(xmlString);
+  final records = document.findAllElements('records');
+}
