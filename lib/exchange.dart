040e08ce05d873118c2459e352890d4f722c1f14
TeofiloAlbuquerque <contatoteofilosantos@gmail.com>
2023-06-06 18:38:38 -0300

    Correção nome delimited do caminho na exchange

--- lib/exchange.dart
+++ lib/exchange.dart
@@ -6,7 +6,7 @@ import 'package:csv/csv.dart';
 import 'package:xml/xml.dart';
 
 part 'data.dart';
-part 'delimeted_data.dart';
+part 'delimited_data.dart';
 part 'xml_data.dart';
 part 'json_data.dart';
 part 'tsv_data.dart';
